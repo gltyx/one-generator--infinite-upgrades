143eceb04553ade472574a3275ca6123465a46c3
veprogames <75524847+veprogames@users.noreply.github.com>
2022-06-21 19:58:07 +0200

    Dev (#1)
    * header and footer
    
    * change number formatting
    
    * add progressive web app, update icons

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const cacheName = "1giu-vepro-mod";
+const assets = [
+    "./",
+
+    "./index.html",
+
+    "./fonts/roboto/RobotoMono-Bold.ttf",
+    "./fonts/roboto/RobotoMono-Bold.woff",
+    "./fonts/roboto/RobotoMono-Bold.woff2",
+    "./fonts/roboto/RobotoMono-Regular.ttf",
+    "./fonts/roboto/RobotoMono-Regular.woff",
+    "./fonts/roboto/RobotoMono-Regular.woff2",
+
+    "./css/fonts.css",
+    "./css/variables.css",
+    "./css/header.css",
+    "./css/footer.css",
+    "./css/style.css",
+
+    "./js/ExpantaNum.js",
+    "./js/script.js",
+    "./js/pwa.js",
+
+    "./icon/icon192x192.png",
+    "./icon/icon512x512.png",
+
+    "./changelog.md"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(assets))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith((async () => {
+        try {
+            const res = await fetch(event.request);
+            (await caches.open(cacheName)).put(event.request, res.clone());
+            return res;
+        }
+        catch (err) {
+            return await caches.match(event.request);
+        }
+    })());
+});
